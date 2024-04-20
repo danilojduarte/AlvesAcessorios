44ba9939c71f59c039e398d83e3da23f6379cd0e
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-04-20 00:10:17 -0300

    Ajustando efeito swipper

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+let swiperCards = new Swiper('.card__content', {
+   loop: true,
+   spaceBetween: 32,
+   grabCursor: true,  
+    
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+
+
 const header = document.querySelector("header");
 
 window.addEventListener ("scroll", function(){

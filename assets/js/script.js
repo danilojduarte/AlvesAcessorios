c82bbed1dddd4be5320ca6fd2463e97b85fafdee
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-04-23 01:53:39 -0300

    Criando novo slide card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,130 +1,117 @@
-let swiperCards = new Swiper('.card__content', {
-   loop: true,
-   spaceBetween: 32,
-   grabCursor: true,  
-    
-    pagination: {
-      el: '.swiper-pagination',
-    },
 
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-});
 
 
 const header = document.querySelector("header");
 
-window.addEventListener ("scroll", function(){
-    header.classList.toggle ("sticky", window.scrollY > 0);
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 // <!-- NOVIDADES -->
-    // <section class="novidades" id="novidades">
-    //     <div class="card__container swiper">
-    //         <div class="card__content">
-    //             <div class="swiper-wrapper">
-    //                 <!-- CARD01 -->
-    //                 <article class="card__article swiper-slide">
-    //                     <div class="card__img">
-    //                         <img src="assets/img/jbl_bluepng_1-removebg-preview.png" alt="">
-    //                         <div class="card__shadow">
-    //                         </div>
+// <section class="novidades" id="novidades">
+//     <div class="card__container swiper">
+//         <div class="card__content">
+//             <div class="swiper-wrapper">
+//                 <!-- CARD01 -->
+//                 <article class="card__article swiper-slide">
+//                     <div class="card__img">
+//                         <img src="assets/img/jbl_bluepng_1-removebg-preview.png" alt="">
+//                         <div class="card__shadow">
+//                         </div>
 
-    //                         <div class="card__data">
-    //                             <h3 class="card__title">Fone de Ouvido</h3>
-    //                             <p class="card__description">
-    //                                 Fone de ouvido bluetooth.
-    //                             </p>
-    //                             <h5 class="price">R$ 70,00</h5>
-    //                             <a href="#" class="card__button">Saiba Mais</a>
-    //                         </div>
+//                         <div class="card__data">
+//                             <h3 class="card__title">Fone de Ouvido</h3>
+//                             <p class="card__description">
+//                                 Fone de ouvido bluetooth.
+//                             </p>
+//                             <h5 class="price">R$ 70,00</h5>
+//                             <a href="#" class="card__button">Saiba Mais</a>
+//                         </div>
 
-    //                     </div>
-    //                 </article>
+//                     </div>
+//                 </article>
 
-    //                 <!-- CARD02 -->
-    //                 <article class="card__article swiper-slide">
-    //                     <div class="card__img">
-    //                         <img src="assets/img/jbl_bluepng_1-removebg-preview.png" alt="">
-    //                         <div class="card__shadow">
-    //                         </div>
+//                 <!-- CARD02 -->
+//                 <article class="card__article swiper-slide">
+//                     <div class="card__img">
+//                         <img src="assets/img/jbl_bluepng_1-removebg-preview.png" alt="">
+//                         <div class="card__shadow">
+//                         </div>
 
-    //                         <div class="card__data">
-    //                             <h3 class="card__title">Nome Produto</h3>
-    //                             <p class="card__description">
-    //                                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-    //                             </p>
-    //                             <h5 class="price">R$ 70,00</h5>
-    //                             <a href="#" class="card__button">Saiba Mais</a>
-    //                         </div>
+//                         <div class="card__data">
+//                             <h3 class="card__title">Nome Produto</h3>
+//                             <p class="card__description">
+//                                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+//                             </p>
+//                             <h5 class="price">R$ 70,00</h5>
+//                             <a href="#" class="card__button">Saiba Mais</a>
+//                         </div>
 
-    //                     </div>
-    //                 </article>
+//                     </div>
+//                 </article>
 
-    //                 <!-- CARD03 -->
-    //                 <article class="card__article swiper-slide">
-    //                     <div class="card__img">
-    //                         <img src="assets/img/jbl_bluepng_1-removebg-preview.png" alt="">
-    //                         <div class="card__shadow">
-    //                         </div>
+//                 <!-- CARD03 -->
+//                 <article class="card__article swiper-slide">
+//                     <div class="card__img">
+//                         <img src="assets/img/jbl_bluepng_1-removebg-preview.png" alt="">
+//                         <div class="card__shadow">
+//                         </div>
 
-    //                         <div class="card__data">
-    //                             <h3 class="card__title">Nome Produto</h3>
-    //                             <p class="card__description">
-    //                                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-    //                             </p>
-    //                             <h5 class="price">R$ 70,00</h5>
-    //                             <a href="#" class="card__button">Saiba Mais</a>
-    //                         </div>
+//                         <div class="card__data">
+//                             <h3 class="card__title">Nome Produto</h3>
+//                             <p class="card__description">
+//                                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+//                             </p>
+//                             <h5 class="price">R$ 70,00</h5>
+//                             <a href="#" class="card__button">Saiba Mais</a>
+//                         </div>
 
-    //                     </div>
-    //                 </article>
+//                     </div>
+//                 </article>
 
-    //                 <!-- CARD04 -->
-    //                 <article class="card__article swiper-slide">
-    //                     <div class="card__img">
-    //                         <img src="assets/img/jbl_bluepng_1-removebg-preview.png" alt="">
-    //                         <div class="card__shadow">
-    //                         </div>
+//                 <!-- CARD04 -->
+//                 <article class="card__article swiper-slide">
+//                     <div class="card__img">
+//                         <img src="assets/img/jbl_bluepng_1-removebg-preview.png" alt="">
+//                         <div class="card__shadow">
+//                         </div>
 
-    //                         <div class="card__data">
-    //                             <h3 class="card__title">Nome Produto</h3>
-    //                             <p class="card__description">
-    //                                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-    //                             </p>
-    //                             <h5 class="price">R$ 70,00</h5>
-    //                             <a href="#" class="card__button">Saiba Mais</a>
-    //                         </div>
+//                         <div class="card__data">
+//                             <h3 class="card__title">Nome Produto</h3>
+//                             <p class="card__description">
+//                                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+//                             </p>
+//                             <h5 class="price">R$ 70,00</h5>
+//                             <a href="#" class="card__button">Saiba Mais</a>
+//                         </div>
 
-    //                     </div>
-    //                 </article>
+//                     </div>
+//                 </article>
 
-    //                 <!-- CARD05 -->
-    //                 <article class="card__article swiper-slide">
-    //                     <div class="card__img">
-    //                         <img src="assets/img/jbl_bluepng_1-removebg-preview.png" alt="">
-    //                         <div class="card__shadow">
-    //                         </div>
+//                 <!-- CARD05 -->
+//                 <article class="card__article swiper-slide">
+//                     <div class="card__img">
+//                         <img src="assets/img/jbl_bluepng_1-removebg-preview.png" alt="">
+//                         <div class="card__shadow">
+//                         </div>
 
-    //                         <div class="card__data">
-    //                             <h3 class="card__title">Nome Produto</h3>
-    //                             <p class="card__description">
-    //                                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-    //                             </p>
-    //                             <h5 class="price">R$ 70,00</h5>
-    //                             <a href="#" class="card__button">Saiba Mais</a>
-    //                         </div>
+//                         <div class="card__data">
+//                             <h3 class="card__title">Nome Produto</h3>
+//                             <p class="card__description">
+//                                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+//                             </p>
+//                             <h5 class="price">R$ 70,00</h5>
+//                             <a href="#" class="card__button">Saiba Mais</a>
+//                         </div>
 
-    //                     </div>
-    //                 </article>
-    //             </div>
-    //         </div>
+//                     </div>
+//                 </article>
+//             </div>
+//         </div>
 
-    //         <!-- Navigation buttons -->
-    //         <div class="swiper-button-prev"></div>
-    //         <div class="swiper-button-next"></div>
+//         <!-- Navigation buttons -->
+//         <div class="swiper-button-prev"></div>
+//         <div class="swiper-button-next"></div>
 
-    //     </div>
-    // </section>
+//     </div>
+// </section>

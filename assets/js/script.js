3612d9c911edf9b7ba34b6cb82a9f840c1378838
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-04-27 11:12:49 -0300

    Ajustando section produtos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,20 @@
 
-
+let swiperCards = new Swiper('.card-produtos', {
+    loop: true,
+    spaceBetween: 32,
+    grabCursos: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 
 const header = document.querySelector("header");
 

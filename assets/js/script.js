56c0cdc116d46a9b1d35710eaa52ca238ffa432f
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2024-05-03 04:57:05 -0300

    Adicionando novas imgs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
-const map = L.map("mapa").setView([-5.7362258540450455, -35.271653090483454],16)
-const layer = L.tileLayer('https://tile.openstreetmap.de/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-});
-
-layer.addTo(map)
-
-
-
-
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function () {
